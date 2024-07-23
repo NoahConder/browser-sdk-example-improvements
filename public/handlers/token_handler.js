eeb0a67d68db46ac8e370d508295414a456dbdd6
Noah <noah.conder@signalwire.com>
2024-07-23 14:01:52 -0500

    Updated the error handling to be more readable

--- public/handlers/token_handler.js
+++ public/handlers/token_handler.js
@@ -1,19 +1,17 @@
-/* ./handlers/token_handler.js */
 const express = require('express');
-require('dotenv').config()
+require('dotenv').config();
 const router = express.Router();
 const axios = require('axios');
-const space = process.env.SIGNALWIRE_SPACE
-const api_key = process.env.SIGNALWIRE_API
-const project = process.env.SIGNALWIRE_PROJECT
+const space = process.env.SIGNALWIRE_SPACE;
+const api_key = process.env.SIGNALWIRE_API;
+const project = process.env.SIGNALWIRE_PROJECT;
 
 const combined = `${project}:${api_key}`;
 const base64Result = Buffer.from(combined).toString('base64');
-router.post("/token_handler", function (req, res) {
 
+router.post("/token_handler", function (req, res) {
     const resource = req.body.verto_resource_box; // Get the user's resource name for their verto endpoint.
     const duration = req.body.jwt_duration;
-
 
     let apiUrl = `https://${space}/api/relay/rest/jwt?resource=${resource}`;
     if (duration !== undefined && duration !== null && duration !== '') {
@@ -33,13 +31,11 @@ router.post("/token_handler", function (req, res) {
     })
         .then(response => {
             console.log("Token successfully generated.");
-            const data = response.data
-            res.json({jwt_token: data.jwt_token, refresh_token: data.refresh_token}); // Send the token and refresh token to the client.
-
-
+            const data = response.data;
+            res.json({ jwt_token: data.jwt_token, refresh_token: data.refresh_token }); // Send the token and refresh token to the client.
         })
         .catch(error => {
-            console.error(error);
+            console.error("Error generating token:", error.response ? error.response.data : error.message);
         });
 });
 
@@ -51,6 +47,7 @@ router.put("/jwt_refresh", function (req, res) {
     if (duration !== undefined && duration !== null && duration !== '') {
         apiUrl += `?expires_in=${duration}`;
     }
+
     axios({
         method: 'put',
         url: apiUrl,
@@ -65,12 +62,11 @@ router.put("/jwt_refresh", function (req, res) {
         .then(response => {
             console.log("Refresh successfully generated.");
             const data = response.data;
-            res.json({jwt_token: data.jwt_token, refresh_token: data.refresh_token});
+            res.json({ jwt_token: data.jwt_token, refresh_token: data.refresh_token });
         })
         .catch(error => {
-            console.error(error);
+            console.error("Error generating token:", error.response ? error.response.data : error.message);
         });
 });
-
 
 module.exports = router;
